7c13aaac2c6a9cc08612e336912cf37c4c8fbcb8
Hamid <xhamid0011@gmail.com>
2019-12-18 18:24:01 +0330

    project currected

--- personal_webpage/personal_webpage/wsgi.py
+++ personal_webpage/personal_webpage/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djangooo project.
+WSGI config for personal_webpage project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangooo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_webpage.settings')
 
 application = get_wsgi_application()
