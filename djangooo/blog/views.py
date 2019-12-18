e35a6928569602b498b28f2a02514b4f60dd802c
Hamid <xhamid0011@gmail.com>
2019-12-18 18:06:30 +0330

    project added

---
+++ djangooo/blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.	# Create your views here.
+
+
+def blog_view(request):
+    return HttpResponse('hey here we go again')
